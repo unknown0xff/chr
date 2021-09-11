8ccb08f0f825294aafc24ed760a029c4e1944bb1
unknown0xff <66771518+unknown0xff@users.noreply.github.com>
2021-09-12 05:17:34 +0800

    fix env issue

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 wget https://download.mikrotik.com/routeros/6.48.1/chr-6.48.1.img.zip -O chr.img.zip && \
 gunzip -c chr.img.zip > chr.img && \
 mount -o loop,offset=512 chr.img /mnt && \
-ADDRESS0=ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1 && \
-GATEWAY0=ip route list | grep default | cut -d' ' -f 3 && \
+ADDRESS0=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+GATEWAY0=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS0 interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY0
 " > /mnt/rw/autorun.scr && \
