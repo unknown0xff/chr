4d4875f22ec7cb98d1845305651ba03d5db9ff9b
unknown0xff <66771518+unknown0xff@users.noreply.github.com>
2022-01-18 19:08:25 +0800

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,5 +1,5 @@
 #!bin/sh
-wget https://download.mikrotik.com/routeros/6.48.1/chr-6.48.1.img.zip -O chr.img.zip && \
+wget https://download.mikrotik.com/routeros/7.1.1/chr-7.1.1.img.zip -O chr.img.zip && \
 gunzip -c chr.img.zip > chr.img && \
 echo u > /proc/sysrq-trigger && \
 dd if=chr.img bs=1024 of=/dev/vda && \
